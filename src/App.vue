8e6b76020736580525ad7e9c66e9f41b902e9f89
Hongtae007 <theerapong6452@gmail.com>
2023-05-10 11:01:50 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name : 'App',
+  data(){
+    return{
+      firstName:"ธีระพงศ์",
+      lastName:"สายอ๋อง",
+      age:20,
+      count: 0,
+      address:"<strong>สงขลา</strong>"
+    }
+  },
+}
+
+</script>
+
+<template>
+<section>
+  <h1>theerapong</h1>
+    <h1>ชื่อ : {{ firstName }}</h1>
+    <h1>สกุล : {{ lastName }}</h1>
+    <h1>อายุ : {{ age }}</h1>
+    <p>ที่อยู่ : <span v-html="address"></span></p>
+  <div id="app">
+    <button @click="count++">
+    Count is: {{ count }}
+    </button>
+  </div>
+  </section>
+  
+</template>
+
+<style>
+
+</style>
